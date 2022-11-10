d2b0d3a5a7f447f15d8f7e087f36b5d5c749741e
tnpfldyd <dkdldhs0322@gmail.com>
2022-11-10 17:03:32 +0900

    Update/accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("signup/", views.signup, name="signup"),
     path("login/", views.login, name="login"),
+    path("kakao/login/callback/", views.kakao_callback, name="kakao_callback"),
     path("login/kakao/", views.kakao_request, name="kakao"),
-    path("login/kakao/callback/", views.kakao_callback, name="kakao_callback"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -69,14 +69,16 @@ def kakao_callback(request):
         "client_id": os.getenv("KAKAO_ID"),
         "redirect_uri": "http://localhost:8000/accounts/kakao/login/callback/",
         "code": request.GET.get("code"),
+        "client_secret": os.getenv("KAKAO_SECRET"),
     }
     kakao_token_api = "https://kauth.kakao.com/oauth/token"
-    access_token = requests.post(kakao_token_api, data=data).json()["access_token"]
+    temp = requests.post(kakao_token_api, data=data).json()
+    access_token = temp["access_token"]
 
     headers = {"Authorization": f"bearer ${access_token}"}
     kakao_user_api = "https://kapi.kakao.com/v2/user/me"
     kakao_user_information = requests.get(kakao_user_api, headers=headers).json()
-
+    print(kakao_user_information)
     kakao_id = kakao_user_information["id"]
     kakao_nickname = kakao_user_information["properties"]["nickname"]
     kakao_profile_image = kakao_user_information["properties"]["profile_image"]
