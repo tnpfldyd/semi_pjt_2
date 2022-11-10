c1221c58c313c421bfb09eea164c4fabf9251761
tnpfldyd <dkdldhs0322@gmail.com>
2022-11-10 09:50:09 +0900

    start

---
+++ semi_pjt_2/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for semi_pjt_2 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'semi_pjt_2.settings')
+
+application = get_asgi_application()
