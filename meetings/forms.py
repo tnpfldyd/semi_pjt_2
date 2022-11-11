e6b874995c0b78c5c682d1f47f1a34301904754b
lsb0709 <dltkdqor0709@gmail.com>
2022-11-11 09:40:59 +0900

    123

--- meetings/forms.py
+++ meetings/forms.py
@@ -8,6 +8,7 @@ class MeetingForm(forms.ModelForm):
             "title",
             "content",
             "image",
+            "location",
             "password",
         ]
 
@@ -15,28 +16,25 @@ class MeetingForm(forms.ModelForm):
             "title": "제목",
             "content": "내용",
             "image": "이미지",
+            "location": "지역",
             "password": "비밀번호",
         }
 
         help_texts = {
-            "title": '이름을 입력해 주세요.',
-            "content": "내용을 입력해 주세요.",
             "image": "이미지를 추가해 주세요.",
-            "password": "비밀번호를 추가해 주세요.",
         }
-
-class UpdateMeetingForm(forms.ModelForm):
-    class Meta:
-        model = Meeting
-        exclude = ['password']
-
-        labels = {
-            "title": "제목",
-            "content": "내용",
-            "image": "이미지",
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["title"].widget.attrs = {
+            "placeholder": "제목을 입력해 주세요.",
         }
-
-
+        self.fields["password"].widget.attrs = {
+            "placeholder": "4자리 비밀번호를 입력해 주세요.",
+        }
+        self.fields["content"].widget.attrs = {
+            "placeholder": "내용을 입력해 주세요.",
+        }
+        self.fields["location"].help_text = None
 
 class CommentForm(forms.ModelForm):
     class Meta:
