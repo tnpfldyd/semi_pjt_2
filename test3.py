3cdcba507ed0c93fde7cd8826f8f91480622b865
tnpfldyd <dkdldhs0322@gmail.com>
2022-11-11 15:24:13 +0900

    Update/delete

--- test3.py
+++ test3.py
@@ -1,15 +1,14 @@
 import json, requests, os
 
 url = "https://kauth.kakao.com/oauth/token"
-headers = {
-    "Content-Type": "application/x-www-form-urlencoded",
-}
 
 data = {
     "grant_type": "refresh_token",
-    "client_id": os.getenv("KAKAO_SECRET"),
-    "refresh_token": "JE0TWb5mlX3SdRGSM29rpKObc4etwUh91HH0DnFnCinJYAAAAYRlCGyr",
+    "client_id": "7e02c2ba46031deb86bc9cd7d6e507f3",
+    "refresh_token": "9y1A_gNs2DHMDxpAGOBFQ8rnflEdnKkDgV6oFVz-Cilw0QAAAYRlN9MP",
+    "client_secret": "ovM5S70qOwngB09GUY2etQgYPTccX8ub",
 }
 
-response = requests.post(url, headers=headers, data=data)
+response = requests.post(url, data=data).json()
+print(os.getenv("KAKAO_ID"))
 print(response)
