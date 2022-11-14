7001d0f4906e9976db4dcd804e690971488044a0
9776mk <9776mk@gmail.com>
2022-11-14 18:15:22 +0900

    create: cards 하나만 추가

--- cards/urls.py
+++ cards/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path("create/group/", views.creategroup, name="creategroup"),
     path("<int:pk>/", views.detail, name="detail"),
     path("<int:pk>/comments/", views.comment_create, name="comment_create"),
+    path("indivdetail/", views.indivdetail, name="indivdetail"),
+    path("<int:pk>/update/", views.cardupdate, name="cardupdate"),
+    path("delete/", views.carddelete, name="carddelete"),
 ]
