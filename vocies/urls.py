d3ec86b162e1fc29249e433da8581db502a20388
lsb0709 <dltkdqor0709@gmail.com>
2022-11-16 17:49:18 +0900

    123

--- vocies/urls.py
+++ vocies/urls.py
@@ -5,8 +5,9 @@ app_name = "vocies"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("myvocie", views.myvocie, name="myvocie"),
+    path("myvocie/", views.myvocie, name="myvocie"),
     path("create/", views.create, name="create"),
+    path("<int:pk>/update/", views.update, name="update"),
     path("<int:pk>/", views.detail, name="detail"),
     path("<int:pk>/comment/", views.comment, name="comment"),
     path("manage_page/", views.manage_page, name="manage_page"),
