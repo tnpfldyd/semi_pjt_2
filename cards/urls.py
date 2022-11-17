9578d098785a95f8f00173339b7225798d33bfcf
tnpfldyd <dkdldhs0322@gmail.com>
2022-11-17 18:28:48 +0900

    Update/base

--- cards/urls.py
+++ cards/urls.py
@@ -8,16 +8,14 @@ urlpatterns = [
     path("create/indiv/", views.create_indiv, name="create_indiv"),
     path("create/group/", views.create_group, name="create_group"),
     # 주소 정리해야함
-    path("detail/group/<int:pk>", views.group_detail, name="group_detail"),
-    path("detail/indiv/<int:pk>", views.indiv_detail, name="indiv_detail"),
+    path("detail/group/<int:pk>/", views.group_detail, name="group_detail"),
+    path("detail/indiv/<int:pk>/", views.indiv_detail, name="indiv_detail"),
     path("<int:pk>/indiv/update/", views.card_update, name="card_update"),
     path("<int:pk>/group/update/", views.groupcard_update, name="groupcard_update"),
     path("<int:pk>/group/delete/", views.groupcard_delete, name="groupcard_delete"),
     path("<int:pk>/usercard/update/", views.usercard_update, name="usercard_update"),
     path("usercard/delete/", views.usercard_delete, name="usercard_delete"),
-    path(
-        "<int:pk>/usercard/comments/", views.usercard_comment, name="usercard_comment"
-    ),
+    path("<int:pk>/usercard/comments/", views.usercard_comment, name="usercard_comment"),
     path("<int:pk>/gcomments/", views.gcomment_create, name="gcomment_create"),
     # path(
     #     "<int:cards_pk>/group/<int:comment_pk>/delete/",
