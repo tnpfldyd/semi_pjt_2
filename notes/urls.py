bd98e41bec1618f196f09e91fc609db773f9f9b6
Myeonghak Lee <mhmh779@naver.com>
2022-11-17 18:11:00 +0900

    notes function

--- notes/urls.py
+++ notes/urls.py
@@ -5,7 +5,7 @@ app_name = "notes"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:username>/send/", views.send, name="send"),
+    path("<int:pk>/send/", views.send, name="send"),
     path("<int:pk>/", views.detail, name="detail"),
     path("<int:pk>/delete/", views.delete, name="delete"),
 ]
