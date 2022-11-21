50ba8fd5999ba62891b907fc1e2895e4111031fd
tnpfldyd <dkdldhs0322@gmail.com>
2022-11-21 21:47:25 +0900

    3

--- semi_pjt_2/settings.py
+++ semi_pjt_2/settings.py
@@ -34,7 +34,7 @@ DEBUG = os.getenv("DEBUG") == "True"
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
-    "http://born-fire-rds.cftxplth8uhy.ap-northeast-2.rds.amazonaws.com",
+    "http://Bornfirebean-env.eba-87fn4uun.ap-northeast-2.elasticbeanstalk.com",
 ]
 
 
