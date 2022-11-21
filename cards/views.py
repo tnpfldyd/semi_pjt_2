1761ff46ed33c30b77042ca94218c7879578f373
tnpfldyd <dkdldhs0322@gmail.com>
2022-11-21 10:20:51 +0900

    update

--- cards/views.py
+++ cards/views.py
@@ -276,7 +276,7 @@ def usercard_comment2(request, pk):
                     "template_object": json.dumps(
                         {
                             "object_type": "text",
-                            "text": request.user.nickname + "님이 트리에 글을 남겨주셨어요.",
+                            "text": request.user.nickname + "님이 벽난로에 글을 남겨주셨어요.",
                             "link": {
                                 "web_url": "http://localhost:8000/cards/detail/usercard/"
                                 + str(pk)
